737b9b5672266e38f26ec3186ab1ac6235c6efb8
Nimeshashaluka <99685297+Nimeshashaluka@users.noreply.github.com>
2024-06-14 10:50:50 +0530

    login page mahi

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,130 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+  Alert,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const Flex = () => {
+  const [text, onChangeText] = React.useState('Enter Your Username');
+  const [password, onChangePassword] = React.useState('Enter Your Password');
 
-export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "ffff",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Text style={styles.text1}>Welcome To අපේ හාමුදුරුවනේ 😌🙏</Text>
+      </View>
+
+      <View style={{ flex: 2 }}>
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={{
+            uri: "https://th.bing.com/th/id/R.02ab23c5bb0e866397fb9a06f04f093c?rik=w9SMHa8RRXMHzw&riu=http%3a%2f%2f3.bp.blogspot.com%2f-CCNnrAwco3w%2fVAaDyav_MmI%2fAAAAAAAAC-g%2fpSJKuqxSMxc%2fs1600%2fmahinda.jpg&ehk=0AJEgdWfJFKlOtvK8svQqNnn0Nx4Db%2fIBfHHMftxUyc%3d&risl=&pid=ImgRaw&r=0",
+          }}
+          style={{ width: 360, height: 250 }}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+
+      <View style={{ flex: 3, backgroundColor: "black", alignItems: "center" }}>
+        <Text style={styles.text2}>
+        Don't think about what happened before, let's start anew, මේ පාර වෙනසක් කරමු ඈ 😹🎉
+        </Text>
+
+        <Button
+          title="[ X ] = 🌹"
+          color="#F44336"
+          onPress={() =>
+            Alert.alert("Test my app", "Hi, my test app is successfuly")
+          }
+        />
+
+        {/* <View style={{ marginTop: 10, backgroundColor: "#ffff" }}>
+          <Text style={styles.text3}>
+            The title and onPress handler are required. It is recommended to set
+            accessibilityLabel to help make your app usable by everyone.
+          </Text>
+        </View> */}
+
+        <View>
+          <Text style={styles.text4}>Join us and Build the Country 🌏🛩️...</Text>
+          <Text style={{color:'#ffff',marginLeft:18,marginTop:15,}}>User Name</Text>
+          <TextInput style={styles.input1} onChangeText={onChangeText} value={text}/>
+        </View>
+        
+        <View>
+          <Text style={{color:'#ffff',marginLeft:18,}}>Password</Text>
+          <TextInput style={styles.input1} onChangeText={onChangePassword} value={password}/>
+        </View>
+
+        <View>
+        <Button
+          title="Sign In"
+          color="green"
+          onPress={() =>
+            Alert.alert("Successe", "Your Data is successfuly Upload")
+          }
+          
+        />
+        
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    // padding: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  text1: {
+    marginTop: 15,
+    padding:10,
+    color: "#ffff",
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 25,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  text2: {
+    color: "#ffff",
+    padding: 10,
+    fontSize: 20,
+    textAlign: "center",
+  },
+  text3: {
+    padding: 10,
+    textAlign: "center",
+    color: "black",
+  },
+  text4: {
+    backgroundColor:'white',
+    fontSize: 18,
+    color: "black",
+    padding: 5,
+    marginTop:10,
+    textAlign: "center",
+  },
+  input1:{
+    color:'#ffff',
+    textAlign:"center",
+    width:300,
+    height:40,
+    margin:12,
+    borderWidth:2,
+    borderColor:'white',
+    borderRadius:10,
   },
 });
+
+export default Flex;
